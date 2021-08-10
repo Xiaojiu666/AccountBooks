7363ffe10cef4357b0b0f028d36b6aa8b9eaf5a3
Gx <417987689@QQ.COM>
2021-08-10 21:42:48 +0800

    [Matrix]

--- app/src/main/java/com/gx/accountbooks/TouchImageVIew.kt
+++ app/src/main/java/com/gx/accountbooks/TouchImageVIew.kt
@@ -2,6 +2,9 @@ package com.gx.accountbooks
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.GestureDetector
+import android.view.GestureDetector.OnGestureListener
+import android.view.MotionEvent
 import androidx.appcompat.widget.AppCompatImageView
 
 class TouchImageVIew : AppCompatImageView {
@@ -9,12 +12,69 @@ class TouchImageVIew : AppCompatImageView {
     constructor(context: Context?, attrs: AttributeSet?) : super(
         context!!,
         attrs
-    ) {}
+    ) {
+    }
 
     constructor(
         context: Context?,
         attrs: AttributeSet?,
         defStyleAttr: Int
     ) : super(context!!, attrs, defStyleAttr) {
+    }
+    var mGestureDetector = GestureDetector(context, MyGestureDetectorListener())
+
+    init {
+        mGestureDetector.setOnDoubleTapListener(MyDoubleTapListener())
+    }
+
+    class MyDoubleTapListener : GestureDetector.OnDoubleTapListener {
+        override fun onDoubleTap(e: MotionEvent?): Boolean {
+
+            return false
+        }
+
+        override fun onDoubleTapEvent(e: MotionEvent?): Boolean {
+            return false
+        }
+
+        override fun onSingleTapConfirmed(e: MotionEvent?): Boolean {
+            return false
+        }
+
+    }
+
+    class MyGestureDetectorListener : OnGestureListener {
+        override fun onShowPress(e: MotionEvent?) {
+        }
+
+        override fun onSingleTapUp(e: MotionEvent?): Boolean {
+            return onSingleTapUp(e)
+        }
+
+        override fun onDown(e: MotionEvent?): Boolean {
+            return false
+        }
+
+        override fun onFling(
+            e1: MotionEvent?,
+            e2: MotionEvent?,
+            velocityX: Float,
+            velocityY: Float
+        ): Boolean {
+            return false
+        }
+
+        override fun onScroll(
+            e1: MotionEvent?,
+            e2: MotionEvent?,
+            distanceX: Float,
+            distanceY: Float
+        ): Boolean {
+            return false
+        }
+
+        override fun onLongPress(e: MotionEvent?) {
+        }
+
     }
 }
